d15a433f1fe365474250f794acfdcc7833baabc2
Archit Badjatya <architj240@gmail.com>
2021-09-05 10:45:55 +0530

    refactoring

--- src/app.js
+++ src/app.js
@@ -11,73 +11,71 @@ const Task = require("./models/task");
 app.use(express.json());
 
 // Creating a user
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
-
-  user
-    .save()
-    .then(() => res.status(201).send(user))
-    .catch((error) => {
-      res.status(401).send(error);
-    });
-});
-
-// Creating a task
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((error) => {
-      res.status(401).send(error);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(401).send(error);
+  }
 });
 
 // Reading Users
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // Reading Single users
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-  User.findById(_id)
-    .then((user) => {
-      if (user) {
-        return res.send(user);
-      } else {
-        return res.status(404).send();
-      }
-    })
-    .catch((e) => {
-      res.status(404).send();
-    });
+// Creating a task
+app.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(401).send(error);
+  }
 });
 
 // Reading All Tasks
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => res.send(tasks))
-    .catch((e) => res.status(500).send());
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // Reading Single Task
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
 
-  Task.findById(_id)
-    .then((user) => res.send(user))
-    .catch((e) => res.status(404).send());
+  try {
+    const task = await Task.findById(_id);
+    res.send(task);
+  } catch (error) {
+    res.status(404).send();
+  }
 });
 
 // Starting Server
