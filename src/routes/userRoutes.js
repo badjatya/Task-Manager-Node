fa756fb10c332d4e9a7943451684008a2f36f75f
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-10-28 21:00:20 +0530

    Geeting user, updating and deleting user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -25,4 +25,48 @@ userRouter.get("/users", async (req, res) => {
   }
 });
 
+// Get User
+userRouter.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(404).send({ error: "User not found" });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// updating user
+userRouter.patch("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      res.status(404).send({ error: "User not found" });
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Delete User
+userRouter.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      res.status(404).send({ error: "User not found" });
+    }
+
+    res.send(user);
+  } catch (error) {}
+});
+
 module.exports = userRouter;
