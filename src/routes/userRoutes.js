ddf96f334af9c1fe7490fee315d223004171d8a8
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-10-28 20:49:17 +0530

    Getting Users

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -15,4 +15,14 @@ userRouter.post("/users", async (req, res) => {
   }
 });
 
+// Get users
+userRouter.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 module.exports = userRouter;
