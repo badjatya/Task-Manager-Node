207f6445cbd005bda2651dd714afd5e5207bd690
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-07 17:40:16 +0530

    rebuilding

--- src/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const databaseURL = "mongodb://127.0.0.1:27017/task-nodejs";
-
-mongoose.connect(databaseURL, {
-    useNewUrlParser:true
-}, () => {
-    console.log("Db connected");
-})
