e9a09880e7f0486320d4e5b1c2448919a065a7c2
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-07 18:07:01 +0530

    Created user model

---
+++ src/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+// Lib
+const validator = require("validator");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Enter a valid email");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Enter valid password");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Enter valid age");
+      }
+    },
+  },
+});
+
+// Model
+const User = mongoose.model("User", userSchema);
+module.exports = User;
