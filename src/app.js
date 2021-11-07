eafa29930374780792e78586ed7afd5f7648a6c5
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-07 18:34:38 +0530

    Created user routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app = express();
 const PORT = 5000 || process.env.PORT;
 require("./db/mongoose");
 
+// Express configuration
+app.use(express.json());
+
+// routes
+app.use(require("./routes/userRouter"));
+
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
 });
---
+++ src/routes/userRouter.js
@@ -0,0 +1,77 @@
+const router = require("express").Router();
+
+// Importing models
+const User = require("../models/user");
+
+// Signup
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// get users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// get single user
+router.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Update user
+router.patch("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Delete user
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ message: "user not found" });
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
