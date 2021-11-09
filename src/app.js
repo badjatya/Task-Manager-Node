e81a66773f51ef70536b235a5a3b02d485978c60
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-09 13:20:33 +0530

    Uploading file

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ require("./db/mongoose");
 // Express configuration
 app.use(express.json());
 
+// Uploading files
+const multer = require("multer");
+const upload = multer({
+  dest: "./src/uploads",
+  limits: 1000000,
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      cb(new Error("Please upload a word document"));
+    }
+
+    cb(undefined, true);
+  },
+});
+
+app.post("/upload", upload.single("upload"), (req, res) => {
+  res.send();
+});
+
 // routes
 app.use(require("./routes/userRouter"));
 app.use(require("./routes/taskRouter"));
